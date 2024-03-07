c057398811e02355234f0dd60009c3392305de6d
Pedro Martinez <pedrocmartinez.dev@gmail.com>
2024-03-06 22:10:02 -0600

    preview modal finished, gallery tab, gallery, info and currency components

--- src/components/preview-modal.tsx
+++ src/components/preview-modal.tsx
@@ -1,8 +1,8 @@
 "use client";
 
 import usePreviewModal from "@/hooks/use-preview-modal";
-// import Gallery from "@/components/gallery";
-// import Info from "@/components/info";
+import Gallery from "@/components/gallery";
+import Info from "@/components/info";
 import Modal from "@/components/ui/modal";
 
 
@@ -21,10 +21,10 @@ const PreviewModal = () => {
     >
       <div className="grid w-full grid-cols-1 items-start gap-x-6 gap-y-8 sm:grid-cols-12 lg:gap-x-8">
         <div className="sm:col-span-4 lg:col-span-5">
-          {/* <Gallery images={product.images} /> */}
+          <Gallery images={product.images} />
         </div>
         <div className="sm:col-span-8 lg:col-span-7">
-          {/* <Info data={product} /> */}
+          <Info data={product} />
         </div>
       </div>
     </Modal>
