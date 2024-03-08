13029a7ff9f24b3e73a22ffc6ad7fb13ed16eba2
Pedro Martinez <pedrocmartinez.dev@gmail.com>
2024-03-07 22:57:00 -0600

    billboard

--- src/app/(routes)/page.tsx
+++ src/app/(routes)/page.tsx
@@ -1,9 +1,22 @@
-import Image from "next/image";
+import getBillboard from "@/actions/get-billboard";
+import Billboard from "@/components/ui/billboard";
+import Container from "@/components/ui/container";
 
-export default function Home() {
+export const revalidate = 0;
+
+export default async function Home(){
+  const billboard = await getBillboard("0e12e5cf-29ab-4529-b8d5-c5371dae1f7b");
+
   return (
-    <main>
-      <h1 className='text-[40px] text-red-500 '>Home</h1>
-    </main>
-  );
-}
+    <Container>
+      <div className="space-y-10 pb-10">
+        <Billboard 
+          data={billboard}
+        />
+        <div className="flex flex-col gap-y-8 px-4 sm:px-6 lg:px-8">
+        </div>
+      </div>
+    </Container>
+  )
+};
+
