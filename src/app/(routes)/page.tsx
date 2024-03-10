33d0a10642632bb48e7484b913e428c7ae9412b3
Pedro Martinez <pedrocmartinez.dev@gmail.com>
2024-03-09 18:43:26 -0600

    add products list

--- src/app/(routes)/page.tsx
+++ src/app/(routes)/page.tsx
@@ -1,11 +1,15 @@
 import getBillboard from "@/actions/get-billboard";
+import getProducts from '@/actions/get-products';
+import ProductList from '@/components/product-list';
 import Billboard from "@/components/ui/billboard";
 import Container from "@/components/ui/container";
 
 export const revalidate = 0;
 
 export default async function Home(){
+  const products = await getProducts({ isFeatured: true });
   const billboard = await getBillboard("65ea9c1d94906e9d03a7469c");
+
   return (
     <Container>
       <div className="space-y-10 pb-10">
@@ -13,6 +17,7 @@ export default async function Home(){
           data={billboard}
         />
         <div className="flex flex-col gap-y-8 px-4 sm:px-6 lg:px-8">
+          <ProductList title="Featured Products" items={products || []} />
         </div>
       </div>
     </Container>
